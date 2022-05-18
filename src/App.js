85ce76c11de439f145862ac812fc4b55cffe8b82
ARyback <adam.ryback@gmail.com>
2022-05-18 16:07:42 -0500

    Successfully developed a simple app to increment numbers

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React, { useState } from 'react';  
 
+// function countInitial() {
+//   return 4;
+// }
+
 function App() {
   const [count, setCount] = useState(4);
+  const [theme, setTheme] = useState('blue');
 
   function decrementCount() {
-    setCount(prevCount => prevCount - 1)
+    setCount(prevCount => prevCount - 1);
+    setTheme('blue');
   }
-  
   function incrementCount() {
-    setCount(prevCount => prevCount + 1)
+    setCount(prevCount => prevCount + 1);
+    setTheme('red');
   }
 
   return (
     <>
       <button onClick={decrementCount}>-</button>
-      <span>{count}</span>
+      <span color={theme}>{count}</span>
+      {/* <span>{theme}</span> */}
       <button onClick={incrementCount}>+</button>
     </>
   )
